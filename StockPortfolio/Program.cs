d83096d92b1f4f3c254bc8198c461a186b2bb0ed
Jfleming <Jfleming07712@gmail.com>
2019-08-26 20:38:43 -0400

    Add project files.

---
+++ StockPortfolio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StockPortfolio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
