bf953c96e0e590fd4c094e03bad1e0303e1e2096
Jfleming <Jfleming07712@gmail.com>
2019-09-09 22:20:24 -0400

    Modified the database a bit with brett tonight.  I have done some restructuring of the C# code that should make the next few parts I am going to work on a bit easier.  I am working on the login stuff now.

--- StockPortfolio/User.cs
+++ StockPortfolio/User.cs
@@ -6,10 +6,13 @@ namespace StockPortfolio
 {
     public class User
     {
+        public int AcctID { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
         public CashAccount CashAccount { get; set; }
         public Portfolio Portfolio { get; set; }
+        public bool LoggedIn { get; set; }
+
 
     }
 }
---
+++ StockPortfolio/UserLoggin.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace StockPortfolio
+{
+    public class UserLoggin
+    {
+        public void LogginUser(User user)
+        {
+            Console.WriteLine("Please enter your Username");
+            var givenUserName = Console.ReadLine();
+
+
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("StockPortfolio")))
+            {
+                var ownedUserNames = connection.Query<User>(
+                    $@"select username
+                        from AccountLoginInfo
+                        where username = '{givenUserName}'").ToList();
+                foreach (User user in RegisteredUsers)
+            }
+        }
+    }
+}
